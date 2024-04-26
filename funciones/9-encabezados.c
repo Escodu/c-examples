91fe9bf8ac8e546db61fe2dab5e1b70cff947190
Escodu <esteban.corrales@ucr.ac.cr>
2024-04-26 00:33:27 -0600

    Incorporación de función 4 en main

--- funciones/9-encabezados.c
+++ funciones/9-encabezados.c
@@ -4,14 +4,18 @@
 int main() {
 
     int v;
+    int s;
 
     // Llama a las 3 funciones
     funcion1();
     funcion2(5, 2.67);
     v = funcion3(10);
+    s = funcion4(10)
 
     // Imprime el valor que retornó funcion3()
-    printf("v = %d\n", v);
+    printf("En la función3 \nv = %d\n", v);
+    printf("En la función4 \nv = %d\n", s);
+
 
     return 0;
 
@@ -31,7 +35,11 @@ void funcion2(int a, float b) {
 // funcion3(): Imprime un mensaje. Retorna el valor que recibió como argumento
 // incrementado en 2
 int funcion3(int x) {
-    printf("En funcion3()...\n");
+    int y = x + 2;
+    return y;
+}
+
+int funcion4(int x) {
     int y = x + 2;
     return y;
 }
